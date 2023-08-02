6518eb5a45ed0fa7ccf14fd91c7d9a2c70f9e1d7
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-02 11:32:42 +0000

    Add NavBarForm and Child

---
+++ my-app/src/components/NavBarChild.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function NavBarChild(props) {
+  return (
+    <div>
+        {!props.isLoggedIn ? (
+            <button onClick={props.handleClick}>login</button>
+            ) : (
+            <form>
+                <label htmlFor = "username">Username:</label>
+                <input placeholder="username" id="username"/>
+            
+                <label htmlFor = "password">Password:</label>
+                <input placeholder="password" id="password"/>
+                <button  onClick={props.handleClick}>Submit</button>
+            </form>
+            )}
+    </div>
+  )
+}
+
+export default NavBarChild
--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -4,9 +4,9 @@ import css from "./css/Sidebar.module.css"
 const Sidebar = () => {
     return (
         <div className={css.sidebar}>
-            <a href="#" target= "_blank">My Photos</a>
-            <a href="#" target= "_blank">My Illustrations</a>
-            <a href="#" target= "_blank">My Paintings</a>
+            <a href="#." target= "_blank">My Photos</a>
+            <a href="#." target= "_blank">My Illustrations</a>
+            <a href="#." target= "_blank">My Paintings</a>
         </div>
     )
 }
