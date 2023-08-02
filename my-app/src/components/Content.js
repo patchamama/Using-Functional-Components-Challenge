610d89e992f7aa6cebf6a11a1c3f1c924685476e
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-08-02 13:07:18 +0000

    Add loading gif

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
+            isLoaded: false
         }
+    }
+
+  componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
 
   render() {
@@ -17,7 +27,14 @@ export class Content extends Component {
         <div className={css.TitleBar}>My Posts</div>
         <div className={css.SearchResults}>
             <div className={css.SearchItem}>
-                <PostItem savedPosts={savedPosts} />
+                {
+                    !this.state.isLoaded ? (
+                        <Loader />
+                    ) : (
+                        <PostItem savedPosts={savedPosts} />
+                    )
+                }
+                
             </div>
         </div>
       </div>
